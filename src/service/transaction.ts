b041e91516997dcbdd3c85ae623c83cd0cfd5cfe
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2024-02-16 00:24:55 -0300

    mock get

--- src/service/transaction.ts
+++ src/service/transaction.ts
@@ -1,11 +1,37 @@
+type extratoBancario = {
+  saldo: {
+    total: number;
+    data_extrato: string;
+    limite: number;
+  };
+  ultimas_transacoes: Array<{
+    valor: number;
+    tipo: 'c' | 'd';
+    descricao: string;
+    realizada_em: string;
+  }>;
+}
+
 export class Transaction {
-  async getExtract() {
-    return {
-      saldo: {
-        total: 1000,
-        tipo: "c",
-        limite: 1000
-      }
-    }
+  async createTransaction() {
+    
+  }
+
+  async getExtract(): Promise<extratoBancario> {
+  const mockData: extratoBancario = {
+    saldo: {
+      total: -1000,
+      data_extrato: "2024-01-17T02:34:41.217753Z",
+      limite: 1000
+    },
+    ultimas_transacoes: [{
+      valor: 10,
+      tipo: "c",
+      descricao: "teste",
+      realizada_em: "2024-01-17T02:34:38.543030Z"
+    }]
+  };
+
+  return mockData;   
   }
 }
